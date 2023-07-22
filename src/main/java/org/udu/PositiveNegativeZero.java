cbf7d8d1499ab0ec67f34ced900f9f39cd108afb
Juan Oliver <juan_oliver@live.se>
2023-07-23 00:52:12 +0200

    Alternative metod update

--- src/main/java/org/udu/PositiveNegativeZero.java
+++ src/main/java/org/udu/PositiveNegativeZero.java
@@ -5,10 +5,14 @@ public class PositiveNegativeZero {
         CheckNumber(5);
         CheckNumber(0);
         CheckNumber(-7);
+        CheckNumber1(5);
+        CheckNumber1(0);
+        CheckNumber1(-7);
     }
 
 
     public static void CheckNumber(int number) {
+
         if (number > 0) {
             System.out.println("positive");
         } else if (number < 0) {
@@ -18,4 +22,12 @@ public class PositiveNegativeZero {
         }
 
     }
+
+    public static void CheckNumber1(int number) {
+
+        System.out.println((number > 0) ? "positive" : ((number < 0) ? "negative" : "zero"));
+
+    }
+
+
 }
