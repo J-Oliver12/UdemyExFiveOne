44a297c9db47970ac64968f9a1712b01019bfa46
Juan Oliver <juan_oliver@live.se>
2023-07-25 18:24:51 +0200

    added testing switch method exercise

--- src/main/java/org/udu/PositiveNegativeZero.java
+++ src/main/java/org/udu/PositiveNegativeZero.java
@@ -21,7 +21,14 @@ public class PositiveNegativeZero {
         printMegaBytesAndKiloBytes(5000);
         printMegaBytesAndKiloBytes(800);
 
-        shouldWakeUp(true,6);
+        System.out.println(shouldWakeUp(true, 6));
+        System.out.println(shouldWakeUp(true, 10));
+        System.out.println(shouldWakeUp(false, 20));
+        System.out.println(shouldWakeUp(true, 23));
+        System.out.println(shouldWakeUp(true, -1));
+        System.out.println(shouldWakeUp(true, 24));
+
+        testingSwitch();
     }
 
 
@@ -59,18 +66,14 @@ public class PositiveNegativeZero {
 
         System.out.println((milesPerHour < 0) ? "Invalid Value" : (kilometersPerHour + " km/h = " + milesPerHour + " mi/h"));
     }
-
     /*     ### Alternative:
     public static void printConversion(double kilometersPerHour) {
-
         long milesPerHour = toMilesPerHour(kilometersPerHour);
-
         if (milesPerHour < 0) {
             System.out.println("Invalid Value");
         } else {
             System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
         }
-
     }
     */
 
@@ -86,7 +89,6 @@ public class PositiveNegativeZero {
 
     }
 
-
         /*
         int kiloByte = 1024;
         int megaByte = 1;
@@ -101,23 +103,48 @@ public class PositiveNegativeZero {
         }
     }
          */
-
 /*
         ### Alternative:
         System.out.println((kiloBytes < 0) ? "Invalid Value" : (kiloBytes +" KB = " + bytesToMega + " MB and "  + modulus + " KB " ));
 */
+
 
     public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
 
         if (hourOfDay < 0 || hourOfDay > 23) {
             return false;
         } else {
-            return barking && (hourOfDay < 8 || hourOfDay >= 23 );
+            return barking && (hourOfDay < 8 || hourOfDay >= 23);
 
         }
     }
 
 
+    public static void testingSwitch() {
+
+        int switchValue = 12;
+
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value is 1");
+                break;
+            case 2:
+                System.out.println("Value is 2");
+                break;
+            case 3: case 4: case 5: case 6: case 7: case 8: case 9: case 10:
+                System.out.println("Value is between 3 and 10");
+                System.out.println("Values is actual: " + switchValue);
+                break;
+            default:
+                System.out.println("Value is not between 1 an 10");
+                System.out.println("Values is actual: " + switchValue);
+        }
+
+    }
+
 }
+
+
+
 
 
